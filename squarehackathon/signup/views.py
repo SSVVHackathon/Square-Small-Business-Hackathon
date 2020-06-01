acdd567a925814be403a00d862a0473992cd4488
SohamGupta21 <62520353+SohamGupta21@users.noreply.github.com>
2020-06-01 16:23:57 -0500

    tejt

--- squarehackathon/signup/views.py
+++ squarehackathon/signup/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from geopy import geocoders
 # Create your views here.
+
+
+g = geocoders.Nominatim()
+
+#include this line within signup
+
+thing = g.geocode(address)
