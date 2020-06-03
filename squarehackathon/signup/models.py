85b28b307755f70453aa935de98682689c205865
Vivek Sharma <vivekcodes.16@gmail.com>
2020-06-03 17:01:19 -0500

    ok

--- squarehackathon/signup/models.py
+++ squarehackathon/signup/models.py
@@ -27,9 +27,9 @@ class MyAccountManager(BaseUserManager):
             username = username,
             address = address,
         )
-        user.is_admin = True
-        user.is_staff = True
-        user.is_superuser = True
+        user.admin = True
+        user.staff = True
+        user.superuser = True
 
         user.save(using=self._db)
         return user
@@ -40,10 +40,10 @@ class Account(AbstractBaseUser):
     username = models.CharField(max_length=30, unique=True)
     date_joined = models.DateTimeField(verbose_name='date joined', auto_now_add=True)
     last_login = models.DateTimeField(verbose_name='last login', auto_now_add=True)
-    is_admin = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)
+    admin = models.BooleanField(default=False)
+    active = models.BooleanField(default=True)
+    staff = models.BooleanField(default=False)
+    superuser = models.BooleanField(default=False)
     
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'address']
@@ -54,7 +54,7 @@ class Account(AbstractBaseUser):
         return self.email
 
     def has_perm(self, perm, obj=None):
-        return self.is_admin
+        return self.admin
     
     def has_module_perms(self, app_label):
         return True
