64395661e8f8f0513643ad724c504c2deb95699f
Socksham <60550018+Socksham@users.noreply.github.com>
2020-06-09 11:40:35 -0500

    models done

--- squarehackathon/main/models.py
+++ squarehackathon/main/models.py
@@ -43,6 +43,10 @@ class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "Order Number: " + str(self.order) + "|Product: " + str(self.product) + "|Quantity: " + str(self.quantity)
+
     @property
     def get_total(self):
         total = self.product.price * self.quantity
