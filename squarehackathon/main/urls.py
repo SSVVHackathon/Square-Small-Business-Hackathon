ea2c03a627254adabc07c45b4eb09ab8562b7d2c
Socksham <60550018+Socksham@users.noreply.github.com>
2020-06-14 16:03:30 -0500

    esgh

--- squarehackathon/main/urls.py
+++ squarehackathon/main/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
-from signup.views import views as signup_views
-from payment.views import views as payment_views
-from .views import home
+from signup import views as signup_views
+from payment import views as payment_views
+from .views import home, process_payment
 
 urlpatterns = [
     path('signup', signup_views.signup_view, name="signup"),
     path('', home, name="home"),
     path('checkout', payment_views.checkout, name="checkout"),
-    path('process-payment', )
+    path('process-payment', process_payment, name="process-payment")
 ]
