5ea20f4b79658806e3147d17029d38e60ca3c227
Socksham <60550018+Socksham@users.noreply.github.com>
2020-06-15 16:13:21 -0500

    square works

--- squarehackathon/main/views.py
+++ squarehackathon/main/views.py
@@ -4,6 +4,9 @@ import json
 from django.views.decorators.csrf import csrf_exempt
 from .models import *
 from signup.models import *
+from square.client import Client
+import uuid
+
 
 # Create your views here.
 def home(request):
@@ -14,7 +17,26 @@ def home(request):
 def process_payment(request):
     data = json.loads(request.body)
     nonce = data['nonce']
-    print(nonce)
+
+    config_type = "SANDBOX"
+    access_token = "EAAAELuuq-CJ1qho_UBpAJrWMlFmA0hTS3s4dKCJOeQa6WmZLsO7PTm-1K-HiGlc"
+
+    client = Client(
+        access_token=access_token,
+        environment="sandbox",
+    )
+
+    idempotency_key = str(uuid.uuid1())
+
+    amount = {'amount': 100, 'currency': 'USD'}
+
+    body = {'idempotency_key': idempotency_key, 'source_id': nonce, 'amount_money': amount}
+
+    api_response = client.payments.create_payment(body)
+    if api_response.is_success():
+        res = api_response.body['payment']
+    elif api_response.is_error():
+        res = "Exception when calling PaymentsApi->create_payment: {}".format(api_response.errors)
 
     return JsonResponse('Item was added', safe=False)
 
